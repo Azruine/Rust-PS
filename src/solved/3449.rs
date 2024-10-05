81ecc28c115920be56ffd504e2f43865042edf05
Azruine <tyriba967@gmail.com>
2024-10-05 22:18:38 +0900

    이게 뭘까

---
+++ src/solved/3449.rs
@@ -0,0 +1,22 @@
+
+fn main()
+{
+    let mut n: String = String::new();
+    std::io::stdin().read_line(&mut n).unwrap();
+    let n: i32 = n.trim().parse().unwrap();
+    for _ in 0..n
+    {
+        let mut t = 0;
+        let mut a = Default::default();
+        let mut b = Default::default();
+        std::io::stdin().read_line(&mut a).unwrap();
+        std::io::stdin().read_line(&mut b).unwrap();
+        a.chars().zip(b.chars()).for_each(|(x, y)| {
+            if x != y
+            {
+                t += 1;
+            }
+        });
+        println!("Hamming distance is {}.", t);
+    }
+}
