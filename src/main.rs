3b6d92ec245f19c767941fcfdf1f4687c48b89cf
Azruine <tyriba967@gmail.com>
2025-01-10 10:20:17 +0900

    한글 변수명? 이걸 어떻게 참아

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+// use std::cmp::Reverse;
+// use std::collections::BinaryHeap;
+// use std::collections::HashMap;
+// use std::io::stdin;
+// use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    // let mut lines = stdin().lock().lines();
+
+    // let line = lines.next().unwrap().unwrap();
+
+    // let mut iter = line.split_whitespace();
+    // let v: i32 = iter.next().unwrap().parse().unwrap();
+    // let e: i32 = iter.next().unwrap().parse().unwrap();
+
+    // let line = lines.next().unwrap().unwrap();
+    // let s: i32 = line.trim().parse().unwrap();
+
+    // let mut graph: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+    // for line in lines {
+    //     let line = line.unwrap();
+    //     let mut iter = line.split_whitespace();
+    //     let u: i32 = iter.next().unwrap().parse().unwrap();
+    //     let v: i32 = iter.next().unwrap().parse().unwrap();
+    //     let w: i32 = iter.next().unwrap().parse().unwrap();
+    //     graph.entry(u - 1).or_default().push((v - 1, w));
+    // }
+
+    // let mut pq = BinaryHeap::from([(Reverse(0), s)]);
+
+    // let mut dist: Vec<Option<i32>> = vec![None; v as usize];
+    // curDist curPos
+    let 문자열: String = String::from("문자열");
+    println!("{문자열}");
 }
